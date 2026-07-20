ec41b26f79478d81381d724b7f551a95525c341c
HΔ5HR <142430965+MASTERRR06@users.noreply.github.com>
2026-07-20 23:43:58 +0530

    Time: 3 ms (10.69%) | Memory: 19.2 MB (60.65%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -1,4 +1,16 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
-        nums.sort()
-        
+        low = 0
+        high = len(nums) - 1
+        curr = 0
+        while curr <= high:
+            if nums[curr] == 0:
+                nums[curr], nums[low] = nums[low], nums[curr]
+                low += 1
+                curr += 1
+            elif nums[curr] == 2:
+               
+                nums[curr], nums[high] = nums[high], nums[curr]
+                high -= 1
+            else:
+                curr += 1
