3763e3f42678f3db10654f32dbfab42db8f05fe9
HΔ5HR <142430965+MASTERRR06@users.noreply.github.com>
2026-07-31 12:50:23 +0530

    Time: 16 ms (97.41%) | Memory: 28.6 MB (74.94%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        buy= float('inf')
+        profit= 0
+        for price in prices:
+            if price<buy:
+                buy=price
+            elif price-buy>profit:
+                profit=price-buy
+        return profit
