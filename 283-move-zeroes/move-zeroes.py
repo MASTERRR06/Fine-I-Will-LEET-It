994408aee1e54b2ac6999685ce49df6037a6f392
HΔ5HR <142430965+MASTERRR06@users.noreply.github.com>
2026-08-01 13:16:05 +0530

    Time: 2 ms (85.54%) | Memory: 20.5 MB (61.63%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.py
@@ -0,0 +1,9 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        r=0 # to start reading from index 1
+        #now what we have to do is compare each number and swap
+        for w in range (len(nums)):# so that it runs the length of the whole array
+            if nums[w]!=0:
+                nums[r],nums[w]=nums[w],nums[r]
+                r+=1
+        
